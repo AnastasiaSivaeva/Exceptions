63f3ddec2e4e631d8650507f50f2133333287a55
Anastasia <anastasia.koles@gmail.com>
2023-04-19 17:28:18 +0300

    Homework

--- Seminar_1/Task1.java
+++ Seminar_1/Task1.java
@@ -2,15 +2,48 @@
 
 package Seminar_1;
 
-public class Task1{
-    public static void main(String[] args) {
+ public class Task1{
+
+    public static void main(String[] arg){
         int num1 = 6;
         int num2 = 0;
-        int result = num1 / num2;
-        if(num1 == 0 || num2 == 0){
-            System.out.println("Ошибка арифметического вычисления. Введите число корректно");
+        if(num1 == 0){
+            System.out.println("Операция деления на 0 невозможна. Введите другое число");
+        }
+        if(num2 == 0){
+            System.out.println("Операция деления на 0 невозможна. Введите другое число");
         }else{
-            System.out.println("Ответ: " +result);
+            System.out.println("Ответ:" +(num1 / num2));
         }
     }
+
+    // public static void main(String[] arg){
+    //     int[] array = {1, 4, 7, 5};
+    //     findInd(array, 5);
+
+
+    // }
+
+    // public static int findInd(int[] arr, int searchInd){
+    //     if (searchInd > arr.length){
+    //         System.out.println("Длина массива меньше заданного индекса");
+    //     }else{
+    //         System.out.println("Условия корректны, значения найдены");
+    //     }
+    //     return 0;
+    // }
+
+//     public static void main(String[] arg){
+//         int size = -4;
+//         int[] array = new int[size];
+//         if(size < 0){
+//             System.out.println("Значение индекса не может быть отрицательным");
+//         }else{
+//             for (int i = 0; i < array.length; i++) {
+//                 array[i] = (int) Math.round(Math.random() * 6);
+//                 System.out.println(array[i]);
+//             }
+//         }
+//     }
+
 }
