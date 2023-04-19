339561c43c06402ea2a28fdf8520b1dfa6ee587f
Anastasia <anastasia.koles@gmail.com>
2023-04-19 18:08:04 +0300

    Homework

--- Seminar_1/Task3.java
+++ Seminar_1/Task3.java
@@ -1,7 +1,7 @@
 package Seminar_1;
 
 public class Task3 {
-    public static void main(int[] arr){
+    public static void main(String[] args){
         int[] array1 = {1, 4, 7, 5};
         int[] array2 = {6, 3, 10, 0};
         CheckArr(array1, array2);
@@ -9,9 +9,7 @@ public class Task3 {
 
     public static int CheckArr(int[] arr1, int[] arr2){
         int dif = 0;
-        if(arr1.length > arr2.length){
-            System.out.println("Ошибка: Длина первого массива отличается от длины второго");
-        }if(arr2.length > arr1.length){
+        if(arr1.length > arr2.length || arr2.length > arr1.length){
             System.out.println("Ошибка: Длина первого массива отличается от длины второго");
         }else{
             for(int i = 0; i < arr1.length; i++){
